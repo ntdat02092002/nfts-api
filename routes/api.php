e3e6cfba3a75e92b0648d0629a092334dbd7bc1f
dat02092002 <ntdat02092002@gmail.com>
2022-10-26 17:05:16 +0700

    add authentication

--- routes/api.php
+++ routes/api.php
@@ -14,12 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('posts', "PostController@index"); // List Posts
-Route::post('posts', "PostController@store"); // Create Post
-Route::get('posts/{id}', "PostController@show"); // Detail of Post
-Route::put('posts/{id}', "PostController@update"); // Update Post
-Route::delete('posts/{id}', "PostController@destroy"); // Delete Post
+Route::group(['middleware' => ['cors', 'json.response']], function () {
+    // public routes
+    Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
+    Route::post('/register','Auth\ApiAuthController@register')->name('register.api');
+    
+    
+    Route::middleware('auth:api')->group(function () {
+        // our routes to be protected will go in here
+        Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+
+        Route::get('posts', "PostController@index"); // List Posts
+        Route::post('posts', "PostController@store"); // Create Post
+        Route::get('posts/{id}', "PostController@show"); // Detail of Post
+        Route::put('posts/{id}', "PostController@update"); // Update Post
+        Route::delete('posts/{id}', "PostController@destroy"); // Delete Post
+    });
+});
+
