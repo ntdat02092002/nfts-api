0468c4a84eeeaeea2558562f79227f5087b21765
dat02092002 <ntdat02092002@gmail.com>
2022-12-24 18:07:23 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,17 +16,18 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@zeno.com',
+            'type' => 1
+        ]);
+        // $this->call([
+        //     PostTableSeeder::class,
         // ]);
-        $this->call([
-            PostTableSeeder::class,
-        ]);
 
-        $this->call([
-            TopicSeeder::class,
-        ]);
+        // $this->call([
+        //     TopicSeeder::class,
+        // ]);
         $this->call([
             CryptoSeeder::class,
         ]);
